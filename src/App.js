183c99146b942ccbdebf58cfacdd42c4b9290336
Jerzy <jurekledzinski@yahoo.pl>
2022-09-09 14:00:49 +0200

    Add access to store list days for calendar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import StoreListDaysProvider from './store/StoreListDays';
 
 import Calendar from './components/Calendar';
 
 const App = () => {
-  return <Calendar></Calendar>;
+  return (
+    <StoreListDaysProvider>
+      <Calendar />
+    </StoreListDaysProvider>
+  );
 };
 
 export default App;
